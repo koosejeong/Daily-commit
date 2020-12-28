fdd128af88d4a825f5a46022cb2ba20281e08945
Qoosj <pcpooh30@gmail.com>
2020-12-28 18:22:33 +0900

    apple watch promotion page make

---
+++ 20201228_afternoon/watch/src/js/common.js
@@ -0,0 +1,6 @@
+function toggle() {
+    const toggle = document.querySelector('.toggle');
+    const banner = document.querySelector('.banner');
+    toggle.classList.toggle('active');
+    banner.classList.toggle('active');
+}
