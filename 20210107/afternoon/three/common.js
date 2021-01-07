bdd2965db59926a5a75f840ec8d6b03d84ff8345
Qoosj <pcpooh30@gmail.com>
2021-01-07 18:44:25 +0900

    background image blur on scroll - using html, css, javascript

---
+++ 20210107/afternoon/three/common.js
@@ -0,0 +1,15 @@
+const blurElem = document.querySelector('.imgBlur');
+
+// function addBlurEffect(){
+//     blurElem.style.filter = "blur(5px)";     
+// };
+
+window.addEventListener('scroll', function(){
+    var scrollPos = document.scrollingElement.scrollTop;
+    console.log(scrollPos);
+    if(scrollPos >= 200){
+        blurElem.classList.add('effect');
+    } else if(scrollPos <= 200) {
+        blurElem.classList.remove('effect');
+    }
+});
