281f2b561fed50c77714f6bfb0b31b739abfef13
qoo se jeong <pcpooh30@gmail.com>
2021-01-10 01:00:12 +0900

    pepsi cola promotion page make

---
+++ 20210109/one/src/js/common.js
@@ -0,0 +1,21 @@
+// let listSel = document.querySelectorAll('.sel>img');
+
+// console.log(listSel);
+
+function imgSlider(any) {
+    document.querySelector('.pepsi').src = any;
+}
+
+// function changeColor
+
+// listSel.forEach(function(list){
+//     list.addEventListener('click', function(list){
+//         if(this === 0) {
+//             document.querySelector('.pepsi').src = './images/pepsi001.png';
+//         } else if(this === 1) {
+//             document.querySelector('.pepsi').src = './images/pepsi002.png';          
+//         } else {
+//             document.querySelector('.pepsi').src = './images/pepsi003.png';  
+//         }
+//     })
+// })
