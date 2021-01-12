be68d1aa7292ee3b0224fe42ce08a57ea7fdaf05
Qoosj <pcpooh30@gmail.com>
2021-01-12 18:25:44 +0900

    mouse move effect

---
+++ 20210112/afternoon/four/common.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener('mousemove', function(e){
+    let body = document.querySelector('body');
+    let heart = document.createElement('span');
+    let x = e.offsetX;
+    let y = e.offsetY;
+    let size = Math.random() * 100;
+    heart.style.left = x+'px';
+    heart.style.top = y+'px';
+    heart.style.width = 20+size+'px';
+    heart.style.height = 20+size+'px';
+    body.appendChild(heart);
+
+    setTimeout(function(){
+        heart.remove();
+    }, 2000);
+});
