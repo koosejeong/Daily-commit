8b32b5cf36ec4f18d0b6c4e92d847006769c0316
Qoosj <pcpooh30@gmail.com>
2021-01-18 11:07:50 +0900

    background animation, CSS & javascript

---
+++ 20210118/morning/one/common.js
@@ -0,0 +1,14 @@
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+const section = document.querySelector('section');
+
+for(let i = 1; i <400; i++) {
+    banner.innerHTML += "<div class='blocks'></div>";
+    const duration = Math.random() * 5;
+    blocks[i].style.animationDuration = 2+duration+'s';
+    blocks[i].style.animationDelay = duration+'s';
+}
+
+setTimeout(function(){
+    section.classList.add('act');
+}, 14000) //add class act after 140secound
